6c22c05a96bfc3f19a75398a635243879dcacbfe
ThiiD <thiago.saber@engenharia.ufjf.br>
2022-04-19 17:08:13 -0300

    Correção de sintaxe

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 
-#define PIN_ACIONAMENTO 9
+#define PIN_ACIONAMENTO 25
 
 void setup(){
     Serial.begin(115200);
     pinMode(PIN_ACIONAMENTO, OUTPUT);
     delay(20000);
 
-    Serial.print("Acionando o Skib...");
+    Serial.println("Acionando o Skib...");
     digitalWrite(PIN_ACIONAMENTO, HIGH);
     delay(200);
     digitalWrite(PIN_ACIONAMENTO, LOW);
